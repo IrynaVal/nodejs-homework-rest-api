bd7aa7524a62f1953121c672aaab7b552350f18f
IrynaVal <piv8050@gmail.com>
2023-06-03 21:18:16 +0300

    added avatar upload

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,5 +1,8 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
 
 const User = require("../models/user");
 
@@ -9,6 +12,8 @@ const { ctrlWrapper } = require("../middlewares");
 
 const { SECRET_KEY } = process.env;
 
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
+
 const register = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
@@ -17,8 +22,13 @@ const register = async (req, res) => {
     throw new HttpError(409, "Email in use");
   }
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     user: {
@@ -82,10 +92,25 @@ const updateSubscription = async (req, res) => {
   res.json(result);
 };
 
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
   updateSubscription: ctrlWrapper(updateSubscription),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const ctrlWrapper = require("./controllerWrapper");
 const validateBody = require("./validateBody");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
 module.exports = {
   ctrlWrapper,
   validateBody,
   authenticate,
+  upload,
 };
